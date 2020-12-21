0b3685c810fbebe52117565f72796223ca3e8018
Andachten <50283838+Andachten@users.noreply.github.com>
2020-12-21 23:17:58 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -10,7 +10,7 @@ import torch as t
 from PIL import Image
 
 
-def pridict(model):
+def predict(model,imgdir):
 
     device = t.device("cuda" if t.cuda.is_available() else "cpu")
     model = model.to(device)
@@ -18,11 +18,9 @@ def pridict(model):
     model.eval()  # 预测模式
 
     # 获取测试图片，并行相应的处理
-    img = Image.open('cat.jpg')
-    transform = transforms.Compose([transforms.Resize(256),  # 重置图像分辨率
-                                    transforms.CenterCrop(224),  # 中心裁剪
+    img = Image.open(imgdir)
+    transform = transforms.Compose([transforms.Resize(224),  # 重置图像分辨率
                                     transforms.ToTensor(), ])
-    img = img.convert("RGB")  # 如果是标准的RGB格式，则可以不加
     img = transform(img)
     img = img.unsqueeze(0)
     img = img.to(device)
@@ -31,9 +29,4 @@ def pridict(model):
         py = model(img)
     _, predicted = t.max(py, 1)  # 获取分类结果
     classIndex_ = predicted[0]
-
-    print('预测结果', classIndex_)
-
-
-if __name__ == '__main__':
-    pridict()
+    return classIndex_.item()
